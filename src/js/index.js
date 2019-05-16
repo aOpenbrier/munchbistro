55a853fa67fa98503c40b8b3e8a0deb5b3e009e8
aOpenbrier <aopenbrier@gmail.com>
2019-05-16 16:34:14 -0700

    separate untappd, nav js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,3 @@
-function openMenu() {
-    document.getElementById('nav-collapse').classList.toggle('open')
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('#navtrigger')) {
-        document.getElementById('nav-collapse').classList.remove('open');
-    }
-}
-
 function openTab(evt, tabId) {
     // Get all elements with class="tabcontent" and hide them
     let tabcontent = document.getElementsByClassName('tabcontent')
@@ -114,13 +103,14 @@ for (const key in menu) {
             let price = item.price ? item.price.toString().split('.')[1] ? item.price.toFixed(2) : item.price : ''
             sectionItem.innerHTML = `
 ${item.price ? `<p class="itemprice">${`${price}`}</p>` : ''}
-<h5 class="itemname">${item.name}</h5>
-<p class="itemdesc">${item.description}</p>
+${item.name ? `<h5 class="itemname">${item.name}</h5>` : ''}
+${item.description ? `<p class="itemdesc">${item.description}</p>` : ''}
 ${item["gf option"] ? `<p class="itemdietary">*Gluten-free optional</p>` : ''}
 ${item.vegan ? `<p class="itemdietary">*Vegan</p>` : ''}
 ${item.vegetarian ? `<p class="itemdietary">*Vegetarian</p>` : ''}
 ${item.extras ? `<p class="itemextras">${item.extras}</p>` : ''}
 ${item.options ? `<p class="itemoptions">${item.options}</p>` : ''}
+${item.list ? `<p class="itemlist">${item.list}</p>` : ''}
 <div class="itemimgwrapper">
     ${item.image ? `
     <div class="itemimage" style="background-image:url(./assets/images/${item.image})">
@@ -135,45 +125,6 @@ ${item.options ? `<p class="itemoptions">${item.options}</p>` : ''}
         document.getElementById(key).appendChild(sectionDiv)
     })
 }
-
-// Get Untappd beer menu
-fetch('https://business.untappd.com/api/v1/menus/62808?full=true', {
-    headers: { "Authorization": "Basic YW9wZW5icmllckBnbWFpbC5jb206RVVYcWFnYnVMN0pEaS1MS3FqUkg=" }
-}).then(r => r.json())
-    .then(r => {
-        console.log(r)
-        let beerMenu = document.createElement('div')
-        r.menu.sections.forEach(section => {
-            let beerSection = document.createElement('div')
-            beerSection.className = 'menusection'
-            beerSection.innerHTML = `<h3 class="sectiontitle">${section.name}</h3>`
-            let sectionBody = document.createElement('div')
-            sectionBody.className = 'sectionbody'
-            section.items.forEach(item => {
-                let sectionItem = document.createElement('div')
-                sectionItem.className = 'sectionitem'
-
-                let containerIndex = 0
-                // check if beer is on tap wall, would include 1 oz pricing
-                item.containers.forEach((container, i) => {
-                    if (container.container_size.ounces == 1.0) {
-                        containerIndex = i
-                    }
-                })
-                sectionItem.innerHTML = `
-            ${item.label_image ? `<a href="https://untappd.com/b/${item.untappd_beer_slug}/${item.untappd_id}"><img class="beerimage" src="${item.label_image}"></a>` : ''}
-<a href="https://untappd.com/b/${item.untappd_beer_slug}/${item.untappd_id}"><h5 class="itemname">${item.name || ''}</h5></a>
-<p class="itemdesc">${item.brewery || ''}, ${item.brewery_location || ''}</p>
-<p class="beerprice">${parseInt(item.containers[containerIndex].container_size.ounces) == 1 ? 'Oz. ' : item.containers[containerIndex].container_size.name || ''} $${item.containers[containerIndex].price || ''}</p>
-`
-                sectionBody.appendChild(sectionItem)
-            })
-            beerSection.appendChild(sectionBody)
-            beerMenu.appendChild(beerSection)
-        })
-
-        document.getElementById('menubeer').appendChild(beerMenu)
-    }).catch(e => console.error(e))
 
 // update arrow indicators after tab interaction is complete
 document.getElementById('menutabs').addEventListener('touchstart', menuScrolled)
